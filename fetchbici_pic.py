262a32b35883546645c838f7edd5c9eeaede97be
xchong <1074974408@qq.com>
2014-09-04 23:16:11 +0800

    to be con

--- fetchbici_pic.py
+++ fetchbici_pic.py
@@ -10,40 +10,49 @@ import os
 
 
 
-
-
 def getimg(html,dirpath):
 
-	drive = "E:\\img\\"+unicode(dirpath,'utf-8')
-	if not os.path.exists(drive):
- 	    os.mkdir(drive)
+    
+    drive = "E:\\img\\"+dirpath.decode('utf-8')#用来解决中文路劲问题
 
-	#过滤出图片的url的list
-	urllist = re.findall(r'<img.*?zoomfile="(.*?.jpg)"',html)
+    if not os.path.exists(drive):
+        os.mkdir(drive)
 
-	for url in urllist:
-		
-		imgname = url[url.rindex('/')+1:]
-		#url_asem 是过滤出的图片url加上比斯的域名
-		url_asem = "http://hkbici.com/"+url
-		print url_asem,imgname
+    #过滤出图片的url的list
+    urllist = re.findall(r'<img.*?zoomfile="(.*?.jpg)"',html)
+
+    for url in urllist:
+        
+        imgname = url[url.rindex('/')+1:]
+        #url_asem 是过滤出的图片url加上比斯的域名
+        url_asem = "http://hkbici.com/"+url
+        print url_asem,imgname
         #从网址获取资源到本地
-		try:
-			urllib.urlretrieve(url_asem, os.path.join(drive,imgname))
-		except:
-			print url_asem+'download failed'
-		
-		
-		
+        try:
+            urllib.urlretrieve(url_asem, os.path.join(drive,imgname))
+        except:
+            print url_asem+'download failed'
+        
+        
+        
 
 #判断是否下载过
 def isexistedurl(title):
     #unicode（）用来解决中文路劲问题
-	if os.path.exists("e:\\img\\"+unicode(title,'utf-8')):
-		return True
-	else:
-		return False
+    if os.path.exists("e:\\img\\"+unicode(title,'utf-8')):
+        return True
+    else:
+        return False
 
+def isinclude(title):
+
+    title_comp = '回復'.decode('utf-8')
+    print title_comp       
+     
+    if title in title_comp:
+        return True
+    else:
+        return False
 
 # Browser
 br = mechanize.Browser()
@@ -84,21 +93,21 @@ br.submit()
 
 
 #fetch the html after log in
-htmlpagexml = br.open('http://hkbici.com/forum-18-7.html').read()
+htmlpagexml = br.open('http://hkbici.com/forum-18-1.html').read()
 print(htmlpagexml)
 
 #magin
 print 'fetch url from website \n'
 
 #filter the page url
-urlpagetitlelist =  re.findall(r'<a href="(thread-.*?)".*?title="(.*?)"',htmlpagexml)
+urlpagetitlelist =  re.findall(r'<a href="(thread-.*?)" title="(.*?)"',htmlpagexml)
 
 #urlpagelist去重复
 newlist = list()
 for item in urlpagetitlelist:
 
-	if  item not in newlist:
-		newlist.append(item) 
+    if  item not in newlist:
+        newlist.append(item) 
 
 #打印出最终的各个图片页面的url
 print newlist
@@ -106,20 +115,24 @@ print newlist
 for urlp,title in newlist:
     
   if isexistedurl(title)==True:
-  	  print "exceed"
-	 
+      print "exceed"
+     
   else:
-  	 #进入详细图片页面的url
-	  urlpasem = "http://hkbici.com/"+urlp
-	  print urlpasem
-	  #将标题解码成中文标题
-	  title.decode('utf-8')	   
-	  print title
-	  #进入详细图片页面url的xml
-	  htmlimgxml = br.open(urlpasem).read()
-	  getimg(htmlimgxml,title)
+     #进入详细图片页面的url
+      urlpasem = "http://hkbici.com/"+urlp
+      print urlpasem
+      #将标题解码成中文标题
+      title.decode('utf-8')
+      print title
+      if isinclude(title):
+          pass 
+      else:
+          #进入详细图片页面url的xml
+          htmlimgxml = br.open(urlpasem).read()
+          getimg(htmlimgxml,title)
 
-	     
+  
+         
 
 
 
