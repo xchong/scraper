539d37a25b0a9e33c174c60044e8b354817275ba
xchong <1074974408@qq.com>
2014-09-05 08:37:00 +0800

    scraper_login.py函数化

--- scrap_login.py
+++ scrap_login.py
@@ -5,54 +5,54 @@ from BeautifulSoup import BeautifulSoup
 import html2text
 
 
+def getloginhtml(login_url,username,password,form_num):
+	
+	# Browser
+	br = mechanize.Browser()
 
-def getloginhtml(url,login_url,username,password,form_num):
-	pass
-# Browser
-br = mechanize.Browser()
+	# Cookie Jar
+	cj = cookielib.LWPCookieJar()
+	br.set_cookiejar(cj)
 
-# Cookie Jar
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
+	# Browser options
+	br.set_handle_equiv(True)
+	br.set_handle_gzip(True)
+	br.set_handle_redirect(True)
+	br.set_handle_referer(True)
+	br.set_handle_robots(False)
 
-# Browser options
-br.set_handle_equiv(True)
-br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
+	# Follows refresh 0 but not hangs on refresh > 0
+	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+	# User-Agent (this is cheating, ok?)
+	br.addheaders = [('User-agent', 'Chrome')]
 
-# User-Agent (this is cheating, ok?)
-br.addheaders = [('User-agent', 'Chrome')]
+	# The site we will navigate into, handling it's session
+	br.open('http://hkbici.com/forum.php')
 
-# The site we will navigate into, handling it's session
-br.open('http://hkbici.com/forum.php')
+	# Inspect name of the form
+	for f in br.forms():
+	    print f
 
-# Inspect name of the form
-for f in br.forms():
-    print f
+	# Select the second (index one) form - the first form is a search query box
+	br.select_form(nr=form_num)
 
-# Select the second (index one) form - the first form is a search query box
-br.select_form(nr=form_num)
+	# User credentials
+	br.form['username'] = username
+	br.form['password'] = password
 
-# User credentials
-br.form['username'] = username
-br.form['password'] = password
+	# Login
+	br.submit()
 
-# Login
-br.submit()
+	print(br.open(login_url).read())
 
-print(br.open(login_url).read())
+if __name__ == '__main__':
+  
 
-if __name__== main:
 
-  url = input('url-> \n')
-  login_url = input('login_url-> \n')
-  username = input('username-> \n')
-  password = input('password-> \n')
-  form_num = input('form_num-> \n')
+  login_url = raw_input('login_url-> \n')
+  username = raw_input('username-> \n')
+  password = raw_input('password-> \n')
+  form_num = int(raw_input('form_num-> \n'))
 
-  getloginhtml(url,login_url,username,password,form_num)
+  getloginhtml(login_url,username,password,form_num)
